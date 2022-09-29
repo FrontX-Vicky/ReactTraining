dd5c9ea433cf72f767cdd00d9c84e3d63a8172ac
Vicky <vicky@tickleright.in>
2022-09-29 10:38:26 +0530

    2022-09-29

--- react_NPM/react-app/src/index.js
+++ react_NPM/react-app/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import AppAPI from './App_api';
-// import RenderProps from './Render_props';
+import AppAPI from './App_api';
+import RenderProps from './Render_props';
 import {ReactRouter, About, Contact, History} from './react_router';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
