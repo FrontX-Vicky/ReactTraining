cd3a60a146ba07c8b0ec947a9e5e974d36a69cce
Vicky <vicky@tickleright.in>
2022-10-08 18:12:17 +0530

    08-10-2022

---
+++ react_NPM/reactinterface/src/App.js
@@ -0,0 +1,12 @@
+import { FaBeer } from 'react-icons/fa';
+
+
+function App() {
+  return (
+    <div className="App container mx-auto mt-5 font-thin">
+      <h1 className='text-5xl'>Hello <FaBeer /></h1>
+    </div>
+  );
+}
+
+export default App;
