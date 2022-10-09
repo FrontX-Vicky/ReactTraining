e863e9c3be3447d3c2489c58019bde891a28d32d
Vicky <vickymore199888@gmail.com>
2022-10-10 00:21:06 +0530

    10-10-2022

--- react_NPM/reactinterface/src/components/Search.js
+++ react_NPM/reactinterface/src/components/Search.js
@@ -6,10 +6,14 @@ const Search = () => {
 
         <div className="py-5">
             <div className="mt-1 relative rounded-md shadow-sm">
-                <div className="absolute inset-y-0 left-0 pl-3 flex">
-
+                <div className="absolute inset-y-0 left-0 pl-3 flex item-center pointer-events-none">
+                    <BiSearch />
+                    <label htmlFor="query" className="sr-only" />
                 </div>
+                <input type="text" name="query" id="query" value=""/>
             </div>
         </div>
     )
 }
+
+export default Search;
