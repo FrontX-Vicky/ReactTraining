ba0f0f862c22601e542f30e0798a7d0da1c82f02
Vicky <vickymore199888@gmail.com>
2022-10-11 00:12:49 +0530

    11-10-2022

--- react_NPM/reactinterface/src/App.js
+++ react_NPM/reactinterface/src/App.js
@@ -1,11 +1,28 @@
-import { FaBeer } from 'react-icons/fa';
+import { BiCalendar } from 'react-icons/bi';
+import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
 import Search from './components/Search';
+import appointmentList from "./data.json"
 
 
 function App() {
   return (
     <div className="App container mx-auto mt-5 font-thin">
+
+      <h2 className='mb-4 text-5xl strong'><BiCalendar className='inline-block text-red-400 align-top' /> My Appointments</h2>
+      <AddAppointment />
       <Search />
+
+      <ul className='divide-y divide-gray-200'>
+        {
+          appointmentList.map(appointment => (
+            <AppointmentInfo 
+            appointment={appointment}
+            />
+          ))
+        }
+
+      </ul>
     </div>
   );
 }
