090cddddaecc13cbd0caff79899b8da4fe99a635
Vicky <vickymore199888@gmail.com>
2022-10-24 00:59:33 +0530

    24-10-2022

--- react_NPM/react-tailwind-table/src/Table.js
+++ react_NPM/react-tailwind-table/src/Table.js
@@ -1,7 +1,41 @@
 // src/Table.js
 import React from "react";
-import { useTable, useGlobalFilter, useAsyncDebounce} from "react-table";
+import { useTable, useGlobalFilter, useAsyncDebounce, useFilters} from "react-table";
 
+//This is a custom filter UI for selecting a unique option from a list
+export function SelectColumnFilter({column :{ filterValue, setFilter, preFilteredRows, id}}){
+    //Calculate the options for filtering using the preFilteredRows
+
+    const options = React.useMemo(() => {
+        const options = new Set();
+        preFilteredRows.forEach((row) => {
+            options.add(row.values[id]);
+        });
+        return [...options.values()];
+    }, [id, preFilteredRows]);
+
+    // render a multi-select box
+
+    return (
+        <select
+            name={id}
+            id={id}
+            value={filterValue}
+            onChange={(e) => {
+                setFilter(e.target.value || undefined)
+            }}
+        >
+            <option value="">All</option>
+            {
+                options.map((option, i)=>(
+                    <option key={i} value={option}>
+                    {option}
+                    </option>
+                ))
+            }
+        </select>
+    )
+}
 
 function GlobalFilter({
     preGlobalFilteredRows,
@@ -29,41 +63,59 @@ function GlobalFilter({
     )
 }
 
-
-
-
 function Table({ columns, data}){
 
     // Use the state and functions returned from useTable to build your UI
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({columns, data});
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, preGlobalFilteredRows, setGlobalFilter } = useTable({columns, data},useFilters, useGlobalFilter);
 
      // Render the UI for your table
     return (
-        <table {...getTableProps()} border="1">
-            <thead>
-                {headerGroups.map((headerGroup)=>(
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => {
-                                return (<td {...cell.getCellProps()}>
-                                    {cell.render("Cell")}
-                                </td>
-                            )})}
+        <>
+            <GlobalFilter 
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+            />
+
+            {headerGroups.map((headerGroup)=>
+            headerGroup.headers.map((column)=>
+            column.Filter ? (
+                <div key={column.id}>
+                    <label for={column.id}>{column.render("Header")}: </label>
+                    {column.render("Filter")}
+                </div> ) : null
+            ))}
+            <table {...getTableProps()} border="1">
+                <thead>
+                    {headerGroups.map((headerGroup)=>(
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                            ))}
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (<td {...cell.getCellProps()}>
+                                        {cell.render("Cell")}
+                                    </td>
+                                )})}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <div>
+                <pre>
+                    <code>{JSON.stringify(state, null, 2)}</code>
+                </pre>
+            </div>
+        </>
     )
 }
 
